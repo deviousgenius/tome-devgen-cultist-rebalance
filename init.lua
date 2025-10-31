0044d3f806370039ef99e3614f5400c9edcc60f2
deviousgenius <devious.genius@hotmail.com>
2025-10-31 13:09:23 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+long_name = "Devgen's Cultist Rebalance"
+short_name = "devgen_cultist_rebalance"
+for_module = "tome"
+version = {1,7,6}
+addon_version = {0, 0, 1}
+weight = 27666
+author = {"devgen"}
+description = [[CoE is a thematically excellent class that isn't as powerful as other classes. I seek to buff the class and offer differing, more powerful playstyles.]]
+tags = {"cultist","class","rebalance"}
+
+hooks = true
+data = false
+overload = false
+superload = true
